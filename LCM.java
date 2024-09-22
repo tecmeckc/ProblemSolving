659fdf0127e3aa22124a803422dd8e62014c0a96
Khyati Choudhary <choudharykhyati49@gmail.com>
2024-09-22 23:29:07 +0530

    adding new file

---
+++ LCM.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class LCM {
+  // function to print LCM of two numbers
+  public static int LCMofij(int i, int j) {
+    int ans = 0, p = Integer.MAX_VALUE;
+    for (int k = (int) Math.min(i, j); k < (i * j); k++) {
+      if (k % i == 0 && k % j == 0) {
+        ans = (int) Math.min(p, k);
+        return ans;
+      }
+    }
+
+    return i * j;
+  }
+
+  public static void main(String args[]) {
+    Scanner sc = new Scanner(System.in);
+    int i = sc.nextInt();
+    int j = sc.nextInt();
+    System.out.println(LCMofij(i, j));
+  }
+}
